d210a83ec8cf15c03490a98e809faec5d803a40f
WilliamBerlin76 <william.berlin76@gmail.com>
2020-07-20 19:25:59 -0500

    using materialui hamburger

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
 
 import { connect, ConnectedProps } from "react-redux";
 
-import { logOut } from "../../actions/index"
+import { logOut } from "../../actions/index";
+import './nav.scss';
 
 const mapState = (state: {
                     user: {
@@ -22,28 +25,50 @@ const connector = connect(mapState, mapDispatch);
 type Props = ConnectedProps<typeof connector>;
 
 const Nav: React.FC<Props> = ({ user, logOut }) => {
-    const [loggedIn, setLoggedIn] = useState<boolean>()
+    const [loggedIn, setLoggedIn] = useState<boolean>();
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
     useEffect(() => {
-        user ? setLoggedIn(true) : setLoggedIn(false)
+        user ? setLoggedIn(true) : setLoggedIn(false);
     }, [user]);
 
+    const handleClick = (e: any) => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <>  
-            {loggedIn && (
-                <>
-                    <NavLink to='/dashboard'>Dashboard</NavLink>
-                    <NavLink to='/' onClick={() => logOut()}>Log Out</NavLink>
-                </>
-            )}
-            {!loggedIn && (
-                <>
-                    <NavLink to='/login'>Login</NavLink>
-                    <NavLink to='/register'>Register</NavLink>
-                </>
-            )}
+        <div className='nav'>  
+            {menuOpen ?
+                <ClearRoundedIcon 
+                    fontSize="large"
+                    onClick={handleClick}
+                    style={{ position: 'fixed', right: '5px', top: '5px'}}
+                />
+                :
+                <MenuRoundedIcon
+                    fontSize="large"
+                    onClick={handleClick}
+                    style={{ position: 'fixed', right: '5px', top: '5px'}}
+                />
+            }
             
-        </>
+            {menuOpen && (
+                <>
+                    {loggedIn && (
+                        <div className='nav-items'>
+                            <NavLink to='/dashboard'>Dashboard</NavLink>
+                            <NavLink to='/' onClick={() => logOut()}>Log Out</NavLink>
+                        </div>
+                    )}
+                    {!loggedIn && (
+                        <div className='nav-items'>
+                            <NavLink to='/login'>Login</NavLink>
+                            <NavLink to='/register'>Register</NavLink>
+                        </div>
+                    )}
+                </>
+            )} 
+        </div>
     );
 };
 

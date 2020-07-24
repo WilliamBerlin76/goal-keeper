82eb83dc092e7a316dcb4026f65ac5b406d3b680
WilliamBerlin76 <william.berlin76@gmail.com>
2020-07-24 18:41:38 -0500

    loader state no longer changes when unmounted

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,38 +1,51 @@
 import React, { useEffect, useState } from 'react';
 
+type LoadTypes = {
+    loading: string;
+    count: number;
+    ascend: boolean
+}
+
 const Loader: React.FC = () => {
     
-    const [loadState, setLoadState] = useState<any>({
+    const [loadState, setLoadState] = useState<LoadTypes>({
                                                         loading: 'Loading',
                                                         count: 0,
                                                         ascend: true
                                                     });
 
     useEffect(() => {
-        
+        let mounted = true;
         setTimeout(() => {
             if(loadState.count < 5 && loadState.ascend === true){
                 let newLoad = '=' + loadState.loading + '=';
-                setLoadState({
-                    ...loadState,
-                    count: loadState.count + 1,
-                    loading: newLoad
-                });
+                if(mounted === true){
+                    setLoadState({
+                        ...loadState,
+                        count: loadState.count + 1,
+                        loading: newLoad
+                    });
+                }
             } else if (loadState.count < 5) {
                 let newLoad = loadState.loading.substr(1, loadState.loading.length - 2);
-                setLoadState({
-                    ...loadState,
-                    count: loadState.count + 1,
-                    loading: newLoad
-                });
+                if(mounted === true){
+                    setLoadState({
+                        ...loadState,
+                        count: loadState.count + 1,
+                        loading: newLoad
+                    });
+                }
             } else {
-                setLoadState({
-                    ...loadState,
-                    count: 0,
-                    ascend: !loadState.ascend
-                });
+                if(mounted === true){
+                    setLoadState({
+                        ...loadState,
+                        count: 0,
+                        ascend: !loadState.ascend
+                    });
+                }
             }
         }, 100);
+        return function(){mounted = false};
     }, [loadState]);
 
     
